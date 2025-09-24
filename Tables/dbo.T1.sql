65d4b42f7caf33cc877f26b585b09d377dcccaea
Hamed Azizi vamerzani <perance.persia@gmail.com>
2025-09-24 18:15:56 +0200

    Update dbo.T1.sql

--- Tables/dbo.T1.sql
+++ Tables/dbo.T1.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[T1]
 (
-[C1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[C1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+  [C2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
